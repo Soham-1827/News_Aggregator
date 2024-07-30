8b174a9a89cedceb5cb5ffc5c057b68bd27c38a4
Soham <118503418+Soham-1827@users.noreply.github.com>
2024-07-30 23:50:41 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,15 +1,15 @@
 import pyrebase
 
 config = {
-  'apiKey': "AIzaSyADCjgV2PeZEpAjPUfWVV60OET-D1zXyQo",
-  'authDomain': "newsaggregator-6b964.firebaseapp.com",
-  'databaseURL': "https://newsaggregator-6b964-default-rtdb.firebaseio.com/",
-  'projectId': "newsaggregator-6b964",
-  'storageBucket': "newsaggregator-6b964.appspot.com",
+  'apiKey': "",
+  'authDomain': "",
+  'databaseURL': "",
+  'projectId': "",
+  'storageBucket': "",
    # 'messagingSenderId': "590901073777",
    # 'appId': "1:590901073777:web:8f2c995011de16ace6c8d0",
    # 'measurementId': "G-FSBZ4ZEJC0",
-    "serviceAccount":"D:/News_Aggregator/service_account_key.json"
+    "serviceAccount":""
 
 }
 firebase = pyrebase.initialize_app(config)
